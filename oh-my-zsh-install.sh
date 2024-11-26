fecef14b2291e805d91b369761012e904e32b70a
Niclas Bjuhr <nrvb@Niclass-MacBook-Pro.local>
2024-11-26 13:50:19 +0100

    added check for oh-my-zsh installation

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed. Skipping installation."
+else
 echo "Installing Oh My Zsh..."
 ZSH_INSTALL_SCRIPT=$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
 echo "$ZSH_INSTALL_SCRIPT" | zsh -s -- --unattended
+fi

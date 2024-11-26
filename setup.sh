b67257ce58c476813031cd6eb095a0865da1051e
Niclas Bjuhr <nrvb@Niclass-MacBook-Pro.local>
2024-11-26 13:58:45 +0100

    added so setup script adds homebrew to path

--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Array of script filenames in the desired execution order
 SCRIPTS=(
-"xcode-cli-setup.sh"
-"oh-my-zsh-install.sh"
-"brew.sh"
-"iterm.sh"
-"set-default-apps.sh"
-"setup-xcode.sh"
-"folders-setup.sh"
+    "xcode-cli-setup.sh"
+    "oh-my-zsh-install.sh"
+    "brew.sh"
+    "iterm.sh"
+    "set-default-apps.sh"
+    "setup-xcode.sh"
+    "folders-setup.sh"
     "macos-settings.sh"
 )
 
@@ -58,6 +58,19 @@ echo ""
 echo "Step 2: Executing setup scripts..."
 for script in "${SCRIPTS[@]}"; do
     execute_script "$SCRIPT_DIR/$script"
+
+    # After brew.sh, update the PATH in the master script's environment
+    if [[ "$(basename "$script")" == "brew.sh" ]]; then
+        echo "Updating PATH in master script after installing Homebrew..."
+        if [ -d "/opt/homebrew/bin" ]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"
+        elif [ -d "/usr/local/bin" ]; then
+            eval "$(/usr/local/bin/brew shellenv)"
+        else
+            echo "Homebrew installation not found in standard locations."
+            exit 1
+        fi
+    fi
 done
 echo "All setup scripts executed successfully."
 echo ""
